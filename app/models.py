96ab5a027782b7a259ee9946883ee798884645ea
Aby-Sebastian <abysebastian98@gmail.com>
2021-05-11 22:16:01 +0530

    code cleanup

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Customer(models.Model):
 	url = models.CharField(max_length=150)
 	short_url = models.CharField(max_length=20, unique=True)
+	title = models.CharField(max_length=150, blank=True, null=True)
 
 	def __str__(self):
 		return self.short_url
--- app/views.py
+++ app/views.py
@@ -9,27 +9,10 @@ import uuid
 import requests
 from bs4 import BeautifulSoup
 
-# Create your views here.
-def index(request):
-	return render(request,'base/index.html')
 
-def login(request):
-	if request.method == 'POST':
-		uname = request.POST.get('email')
-		pwd = request.POST.get('password')
-		print(uname,pwd)
-		# user = authenticate()
-	return render(request, 'base/login.html')
-
-def register(request):
-
-	return render(request, 'base/register.html')
-
-def dashboard(request):
-	return render(request, 'dashboard/dashboard.html')
 	
 def links(request):
-    links = Customer.objects.all()
+    links = Customer.objects.all().order_by('-id')
     form = CreateCustomLinkForm()
     context = {'links':links, 'form':form}
     return render(request, 'dashboard/links.html',context)
@@ -52,7 +35,7 @@ def save_data(request):
             form = CreateCustomLinkForm(request.POST, instance=sid)
 
         uid = str(uuid.uuid4())[:5]
-        print("here-----------------",lid)
+        # print("here-----------------",lid)
 
         short_url = request.POST['short_url']
         if short_url == '':
@@ -63,7 +46,7 @@ def save_data(request):
             value=request.POST['url']
             print(lid,value,short_url,sep=" ")
             print("----------------------Start--------------------------")
-            # print(scrape_title(value))
+            # title = scrape_title(value)
             
             
             expiration = timezone.now() + timezone.timedelta(days=30)
@@ -75,10 +58,10 @@ def save_data(request):
 
                 custom_url = Customer(id=lid,url=value,short_url=short_url)
             custom_url.save()
-            cust = Customer.objects.values()
+            cust = Customer.objects.values().order_by('-id')
             # print(cust)
             customer_values = list(cust)
-            return JsonResponse({'status': 'save', 'customer_values':customer_values })
+            return JsonResponse({'status': 'save', 'customer_values':customer_values})
         else:
             return JsonResponse({'status': 0})
 
